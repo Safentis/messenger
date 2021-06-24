e526c8187595db96ee5ad41cf066a3a25ce4169a
Safentis <fryazinovanton@gmail.com>
2021-06-25 02:28:13 +0300

    MES-25 Added momont library and created last activity in the chat

--- src/layouts/Chat/ChatNamebar/ChatNamebar.tsx
+++ src/layouts/Chat/ChatNamebar/ChatNamebar.tsx
@@ -5,6 +5,7 @@ import './ChatNamebar.css';
 import { FontAwesomeIcon    } from '@fortawesome/react-fontawesome';
 import { faPlus             } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch        } from 'react-redux';
+import moment                 from 'moment'
 
 import Button                 from '../../../components/Button/Button';
 import Status                 from '../../../components/Status/Status';
@@ -12,14 +13,17 @@ import { requestSaveDialogs } from '../../../redux/actionCreators/dialogs';
 
 const ChatNamebar: FC <Props> = (dialog): any => {
        
-    const { client, online }: Props = dialog;
+    const { client, online, timestamp }: Props = dialog;
     const dispatch: any = useDispatch();
     
     const handleSave = () => {
         dispatch(
             requestSaveDialogs(dialog)
         );
-    }; 
+    };
+
+    const date: any = moment(timestamp);
+    const now : number = date.fromNow();
 
     return (
         <section className="namebar">
@@ -29,9 +33,12 @@ const ChatNamebar: FC <Props> = (dialog): any => {
                     <h2 className="namebar__username">
                         {client}
                     </h2>
+                    <p className="namebar__last-visit">
+                        last activity: {now}
+                    </p>
                 </div>
                 <div className="namebar__functionality">
-                    <Button className="namebar__button" onClick={handleSave}>
+                    <Button className="namebar__button" onClick={handleSave} title="Save dialog">
                         <FontAwesomeIcon className="icon icon_white" icon={faPlus}/>
                     </Button>
                 </div>
