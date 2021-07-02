e8706bd49846a50747b901ccc7f094e1ba7157f2
Safentis <fryazinovanton@gmail.com>
2021-07-02 04:06:58 +0300

    MES-33 Created components for displaying messages in a dialog.

--- src/components/Message/Message.interface.ts
+++ src/components/Message/Message.interface.ts
@@ -1,5 +1,6 @@
 export interface Props {
     content  : string
-    timestamp: string, 
+    timestamp: string 
     writtenBy: string
+    src      : string[]
 }
--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -5,7 +5,7 @@ import './Message.css';
 import Avatar        from '../Avatar/Avatar';
 import moment from 'moment';
 
-const Message: FC <Props> = ({content, timestamp, writtenBy}): any => {
+const Message: FC <Props> = ({content, timestamp, writtenBy, src}): any => {
     const date: any = moment(timestamp);
     const lastActivity: string = date.fromNow();
     
@@ -14,11 +14,25 @@ const Message: FC <Props> = ({content, timestamp, writtenBy}): any => {
     const contentClass: string = isClient ? 'message__content_grey' : 'message__content_blue';
     const avatarClass : string = isClient ? 'avatar_left' : 'avatar_right'
 
+    const images: any[] = src?.map((path: string, index: number) => 
+        <img 
+            className="message__img" 
+            key={index}
+            src={path} 
+            alt="image" 
+        />
+    );
+
+    const IMAGES: any = (
+        images?.map(img => img)
+    );
+
     return (
         <div className={`message ${messageClass}`}>
             <p className={`message__content ${contentClass}`}>
                {content}
             </p>
+            {IMAGES}
             <div className="message__user">
                 <Avatar className={`message__user-avatar ${avatarClass}`} width="50" height="50">
                     <p className="message__user-last-activity">
